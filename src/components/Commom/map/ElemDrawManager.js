0f1bab0be6ee9aa086c19fca62cf8b973e86cd70
ueredeveloper <ueredeveloper@gmail.com>
2023-07-06 08:12:34 -0300

    melhorando infowindow

--- src/components/Commom/map/ElemDrawManager.js
+++ src/components/Commom/map/ElemDrawManager.js
@@ -3,7 +3,7 @@ import { useContext, useEffect } from 'react';
 import { findPointsInsidePolygon, findPointsInsideRectangle, findPointsInsideCircle, findAllPointsInRectangle, findAllPointsInPolygon, findAllPointsInCircle } from '../../../services/geolocation';
 import { SystemContext } from '../../MainFlow/Analyse';
 import { calculateCircleArea, calculatePolygonArea, calculatePolylineLength, calculateRectangleArea, numberWithCommas } from '../../../tools';
-import ElemInfoWindow from './ElemInfoWindow';
+
 import redIcon from '../../../assets/red-icon.png'
 /**
 * Adiciona marcador, círculo, polígono, poliline e retângulo ao mapa.
@@ -56,26 +56,6 @@ const ElemDrawManager = ({ map }) => {
           icon: { url: redIcon, scaledSize: new window.google.maps.Size(30, 30) }
         });
         let position = marker.position;
-
-
-        let content = `
-          <div >
-            <h3> Informações do Marcador <h3/>
-            <div style="font-size: 12px">
-                <p> Coordenadas: ${position.lat()}, ${position.lng()}</p>
-            </div>   
-          </div>`
-/*
-        let infowindow = new window.google.maps.InfoWindow({
-          content: content,
-        });
-
-        marker.addListener("click", () => {
-          infowindow.open({
-            anchor: marker,
-            map,
-          });
-        });*/
 
         setMarker(prev => {
           return {
@@ -209,15 +189,6 @@ const ElemDrawManager = ({ map }) => {
           area: calculateRectangleArea(event.overlay.getBounds()),
           markers: await findAllPointsInRectangle(SW.lng(), SW.lat(), NE.lng(), NE.lat())
         }
-/*
-        console.log(shape)
-        // Mostrar informações do polígono
-        let infowindow = new window.google.maps.InfoWindow({
-          content: setContent(shape),
-          map
-        });
-        infowindow.setPosition({ lat: lat, lng: lng });
-        infowindow.setMap(map);*/
 
         setOverlays(prev => {
           return {
@@ -257,15 +228,6 @@ const ElemDrawManager = ({ map }) => {
 
         }
 
-        /*
-        let infowindow = new window.google.maps.InfoWindow({
-          content: setContent(shape),
-          map
-        });
-        infowindow.setPosition({ lat: lat, lng: lng });
-        infowindow.setMap(map);
-        */
-
         setOverlays(prev => {
           return {
             ...prev,
@@ -279,93 +241,9 @@ const ElemDrawManager = ({ map }) => {
 
   }, [map]);
 
-
   return null;
 
 };
-
-const setContent = (draw) => {
-
-  if (draw.type === 'polyline') {
-    let coordinates = [];
-    let htmlCoords = '';
-
-    draw.draw.getPath().forEach((latLng) => {
-      coordinates.push({ lat: latLng.lat(), lng: latLng.lng() });
-    });
-    coordinates.forEach((coordinate, index) => {
-      htmlCoords += `${index + 1}: ${coordinate.lat}, ${coordinate.lng}<br>`;
-    });
-    /* conversão: 1000 metros = 1km */
-    let meters = draw.meters.toFixed(3);
-    let formatMeters = numberWithCommas(meters)
-    let km = draw.meters / 1000;
-    let formatKm = numberWithCommas(km)
-
-    return `
-          <div style="overflow-y: scroll; padding: 0px; height: 4rem;  width: 20rem">
-              <h3> Informações da Polilinha </h3>
-              <div style="font-size: 12px">
-                  <b> ${formatMeters} metros = ${formatKm} km </b>
-              <br/>
-              ${htmlCoords}
-              </div>
-          </div>
-      `
-  }
-  if (draw.type === 'rectangle') {
-    /* conversão: 1.000.000 Metros quadrados = 1 Quilômetros quadrados  */
-
-    let areaM2 = draw.area.toFixed(2)
-    let formatAreaM2 = numberWithCommas(areaM2)
-    let areakKm2 = draw.area / 1000000
-    let formatAreaKm2 = numberWithCommas(areakKm2)
-    return `
-          <div style="overflow-y: scroll; height: 4rem; width: 20rem">
-              <h3> Informações do Retângulo </h3>
-              <div style="font-size: 12px">
-                  <b> Área: ${formatAreaM2} m² = ${formatAreaKm2} km² </b>
-              </div>
-          </div>
-      `
-  }
-  if (draw.type === 'polygon') {
-    /* conversão: 1.000.000 Metros quadrados = 1 Quilômetros quadrados  */
-  
-    let areaM2 = draw.area.toFixed(2)
-    let formatAreaM2 = numberWithCommas(areaM2)
-    let areakKm2 = draw.area / 1000000
-    let formatAreaKm2 = numberWithCommas(areakKm2)
-    return `
-          <div style="overflow-y: scroll; height: 4rem; width: 20rem">
-              <h3> Informações do Polígono </h3>
-              <div style="font-size: 12px">
-                  <b> Área: ${formatAreaM2} m² = ${formatAreaKm2} km² </b>
-              </div>
-          </div>
-      `
-  }
-  if (draw.type === 'circle') {
-    /* conversão: 1.000.000 Metros quadrados = 1 Quilômetros quadrados  */
-
-    let formatAream2 = numberWithCommas(draw.area)
-    let km2 = draw.area / 1000000
-    let formatKm2 = numberWithCommas(km2)
-    let formatRadius = numberWithCommas(draw.radius)
-    return `
-          <div >
-              <h3> Informações do Círculo <h3/>
-              <div style="font-size: 12px">
-                  <p> Área: ${formatAream2} m² = ${(formatKm2)} km²</p>
-                  <b> Raio: ${formatRadius} metros</p>
-              </div>   
-          </div>
-      `
-  }
-  return `<div></div>`
-}
-
-
 
 export default ElemDrawManager;
 
