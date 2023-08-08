09f51b9f4ad7f05c655551fc3798d77b9c83a658
ueredeveloper <ueredeveloper@gmail.com>
2023-08-08 10:25:03 -0300

    adicionando bacia e uh no infowindow

--- src/components/Commom/map/ElemInfoWindow.js
+++ src/components/Commom/map/ElemInfoWindow.js
@@ -1,3 +1,12 @@
+/**
+ * Componente para exibir informações em uma janela de informações associada a um marcador no mapa.
+ *
+ * @param {object} props - Propriedades do componente.
+ * @param {google.maps.Marker} props.marker - O marcador ao qual a janela de informações está associada.
+ * @param {object} props.info - As informações a serem exibidas na janela.
+ * @param {google.maps.Map} props.map - O mapa ao qual o marcador e a janela de informações pertencem.
+ * @returns {null} Retorna null, pois este componente não possui renderização visível.
+ */
 import { createRef, useEffect, useRef, useState } from 'react';
 import { iwManualIcon, iwTubularIcon, iwSuperficialIcon, iwBarragemIcon, iwEfluenteIcon, iwPluvialIcon } from '../../../assets/svg/svgs-icons';
 import { setInfoMarkerIcon } from '../../../tools';
@@ -5,28 +14,48 @@ import { useTheme } from '@mui/material/styles';
 
 const ElemInfoWindow = ({ marker, info, map }) => {
 
+    /**
+     * Estado que controla a instância da janela de informações.
+     * @type {google.maps.InfoWindow}
+     */
     const [infowindow, setInfowindow] = useState();
+
+    /**
+     * Estado que controla se a janela de informações está aberta ou fechada.
+     * @type {boolean}
+     */
     const [isOpen, setIsOpen] = useState(false);
+
+    /**
+     * Referência a um elemento para o efeito de hover.
+     * @type {React.RefObject}
+     */
     const hoverEffectRef = useRef(null);
+
+    /**
+     * Estado que controla o elemento div a ser renderizado na janela de informações.
+     * @type {HTMLElement}
+     */
     const [divElement, setDivElement] = useState(null);
 
+    /**
+     * O tema do Material-UI sendo utilizado no componente.
+     */
     const theme = useTheme();
     
     useEffect(() => {
-
+        // Cria a instância da janela de informações caso ainda não exista.
         if (!infowindow) {
-
             let _infowindow = new window.google.maps.InfoWindow({
                 content: setContent(theme.palette.primary.main, info),
-                
             });
-            // sobrepor infowindow ao popup do polígono, retângulo etc...
+            // Sobrepor a janela de informações ao popup do polígono, retângulo, etc...
             _infowindow.setZIndex(10);
 
-
+            // Adiciona um listener para abrir a janela de informações quando o marcador for clicado.
             if (marker) {
                 marker.addListener("click", () => {
-                    console.log('listener marker click')
+                    console.log('listener marker click');
                     _infowindow.open({
                         anchor: marker,
                         map: map,
@@ -37,42 +66,44 @@ const ElemInfoWindow = ({ marker, info, map }) => {
             setInfowindow(_infowindow);
         }
 
-
+        // Atualiza a posição da janela de informações ao marcador.
         if (map && marker && infowindow) {
-
             infowindow.setPosition(marker.position);
         }
+    }, [map, marker, infowindow]);
 
-
-    }, [map, marker, infowindow])
-
+    // Retorna null, pois este componente não possui renderização visível.
     return null;
 };
 
-
-
+/**
+ * Função que gera o conteúdo da janela de informações.
+ *
+ * @param {string} color - A cor de fundo da janela de informações.
+ * @param {object} info - As informações a serem exibidas.
+ * @returns {HTMLElement} Retorna o elemento div com o conteúdo da janela de informações.
+ */
 const setContent = (color, info) => {
 
+    // Obtém os dados do ícone para a janela de informações.
     let svgData = setInfoMarkerIcon(info.id, info.ti_id, info.tp_id).iw;
 
-    // Create an object element
-
+    // Cria um elemento object para renderizar a imagem SVG.
     const image = document.createElement('object');
     image.setAttribute('type', 'image/svg+xml');
     image.setAttribute('data', `data:image/svg+xml,${encodeURIComponent(svgData)}`);
-
     image.style.width = '70px';
     image.style.height = '70px';
 
-
-    // Create the container div
+    // Cria o elemento div para o container da janela de informações.
     const containerDiv = document.createElement('div');
     containerDiv.id = 'wi-container';
 
-    // Create the title div
+    // Cria o elemento div para o título.
     const titleDiv = document.createElement('div');
     titleDiv.id = 'wi-title';
 
+    // Cria os elementos div para exibir o tipo e descrição.
     const tittleType = document.createElement('div');
     tittleType.textContent = info.ti_descricao;
     const wellType = document.createElement('div');
@@ -80,10 +111,10 @@ const setContent = (color, info) => {
     titleDiv.appendChild(tittleType);
     titleDiv.appendChild(wellType);
 
-    // Create a <style> element
+    // Cria um elemento <style> para definir os estilos CSS.
     const styleElement = document.createElement('style');
 
-    // Set the CSS styles
+     // Set the CSS styles
 
     const setStyles = (bgColor) => {
         return `
@@ -126,67 +157,78 @@ const setContent = (color, info) => {
           }
         `;
     };
-
+    
     styleElement.textContent = setStyles(color);
 
-    // Append the <style> element to the <head> of the document
+    // Adiciona o <style> ao <head> do documento.
     document.head.appendChild(styleElement);
 
+    // Cria o elemento div para o conteúdo rolável.
     const overlflowDiv = document.createElement('div');
     overlflowDiv.id = 'wi-overflow';
 
-    // Create the subtitle div
+    // Cria o elemento div para o subtítulo.
     const subtitleDiv = document.createElement('div');
     subtitleDiv.id = 'wi-subtitle';
 
-    // Create the first inner div of the subtitle
+    // Cria o primeiro div interno do subtítulo.
     const infoDiv = document.createElement('div');
     infoDiv.textContent = 'Informações';
 
-    // Append the inner divs to the subtitle div
+    // Adiciona os elementos div ao subtítulo.
     subtitleDiv.appendChild(infoDiv);
     subtitleDiv.appendChild(image);
 
-
-
-    // Create the info div
+    // Cria o elemento div para o conteúdo das informações.
     const infoContentDiv = document.createElement('div');
-    infoContentDiv.id = 'wi-info'
+    infoContentDiv.id = 'wi-info';
     const infoTextDiv = document.createElement('div');
 
-    // Create the <p> elements for each property and set their text content
-    const nomeElement = document.createElement('p');
-    nomeElement.textContent = `Nome: ${info.us_nome}`;
+    // Cria os elementos <p> para cada propriedade e define o conteúdo de texto.
+    const nome = document.createElement('p');
+    nome.textContent = `Nome: ${info.us_nome}`;
 
-    const cpfElement = document.createElement('p');
-    cpfElement.textContent = `CPF: ${info.us_cpf_cnpj}`;
+    const cpfCnpj = document.createElement('p');
+    cpfCnpj.textContent = `CPF: ${info.us_cpf_cnpj}`;
 
-    const enderecoElement = document.createElement('p');
-    enderecoElement.textContent = `Endereço: ${info.emp_endereco}`;
+    const numAto = document.createElement('p');
+    numAto.textContent = `Número do Ato: ${info.int_num_ato}`;
 
-    const processoElement = document.createElement('p');
-    processoElement.textContent = `Processo: ${info.int_processo}`;
+    const endereco = document.createElement('p');
+    endereco.textContent = `Endereço: ${info.emp_endereco}`;
 
-    const coordenadasElement = document.createElement('p');
-    coordenadasElement.textContent = `Coordenadas: ${info.int_latitude}, ${info.int_longitude}`;
+    const processo = document.createElement('p');
+    processo.textContent = `Processo: ${info.int_processo}`;
 
-    infoTextDiv.appendChild(nomeElement);
-    infoTextDiv.appendChild(cpfElement);
-    infoTextDiv.appendChild(enderecoElement);
-    infoTextDiv.appendChild(processoElement);
-    infoTextDiv.appendChild(coordenadasElement);
+    const coordenadas = document.createElement('p');
+    coordenadas.textContent = `Coordenadas: ${info.int_latitude}, ${info.int_longitude}`;
 
+    const bacia = document.createElement('p');
+    bacia.textContent = `Bacia Hidrográfica: ${info.bh_nome}`;
+
+    const unidade = document.createElement('p');
+    unidade.textContent = `Unidade Hidrográfica: ${info.uh_nome}`;
+
+    // Adiciona os elementos <p> ao div de texto das informações.
+    infoTextDiv.appendChild(nome);
+    infoTextDiv.appendChild(cpfCnpj);
+    infoTextDiv.appendChild(numAto);
+    infoTextDiv.appendChild(endereco);
+    infoTextDiv.appendChild(processo);
+    infoTextDiv.appendChild(coordenadas);
+    infoTextDiv.appendChild(bacia);
+    infoTextDiv.appendChild(unidade);
+
+    // Adiciona o div de texto das informações ao div de conteúdo.
     infoContentDiv.appendChild(infoTextDiv);
 
-    // Append all the elements to the container div
+    // Adiciona todos os elementos ao div do container.
     containerDiv.appendChild(titleDiv);
     overlflowDiv.appendChild(subtitleDiv);
     overlflowDiv.appendChild(infoContentDiv);
-
     containerDiv.appendChild(overlflowDiv);
 
-
-
+    // Retorna o elemento div completo.
     return containerDiv;
 }
 
---
+++ src/docs/semana-2-ago-23.md
@@ -0,0 +1,2 @@
+1 - Unidades Hidrográficas e Bacias Hidrográficas
+    A Rosângela pediu as informações das UHs e BHs no inforwindow de cada marcador (outorga). Verificar se está sendo trazido o código de cada UH e BH e não só o nome. Lembrando que  o código não é  o mesmo que o id de cada bacia.
