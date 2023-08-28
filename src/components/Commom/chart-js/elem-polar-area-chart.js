24782575b373aa69b7095c143ec75f6ceb882e65
ueredeveloper <ueredeveloper@gmail.com>
2023-08-28 07:51:57 -0300

    começando a buscar o hidden de cada label

--- src/components/Commom/chart-js/elem-polar-area-chart.js
+++ src/components/Commom/chart-js/elem-polar-area-chart.js
@@ -13,35 +13,17 @@ import { initialState } from '../../../initial-state';
 
 ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
-export const data = {
-    labels: ['Subterrâneo', 'Superficial', 'Barragem', 'Pluviais', 'Efluentes'],
-    datasets: [
-        {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.5)',
-                'rgba(54, 162, 235, 0.5)',
-                'rgba(255, 206, 86, 0.5)',
-                'rgba(75, 192, 192, 0.5)',
-                'rgba(153, 102, 255, 0.5)',
-                'rgba(255, 159, 64, 0.5)',
-            ],
-            borderWidth: 1,
-        },
-    ],
-};
-
 export function ElemPolarAreaChart() {
 
 
     const [marker, setMarker, overlays, setOverlays] = useContext(AnalyseContext);
-    const [markersLen, setMarkersLen] = useState({
+    const [numberOfGrants, setNumberOfGrants] = useState({
         labels: ['Subterrânea', 'Superficial', 'Pluviais', 'Efluentes', 'Barragem'],
         datasets: [
             {
                 label: '',
-                data: [50, 100, 70, 20, 10],
+                data: [100, 50, 25, 70, 60],
+                
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.5)',
                     'rgba(54, 162, 235, 0.5)',
@@ -50,19 +32,40 @@ export function ElemPolarAreaChart() {
                     'rgba(153, 102, 255, 0.5)'
                 ],
                 borderWidth: 1,
+                //hidden: [false, false, false, false, false],
+                
             },
         ],
-        scales: {
-            y: {
-                // uso de escala logarítmica ou não
-                type: 'logarithmic',
-                // position: 'left',
 
-            },
-        },
     });
 
+    const options = {
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: (context) => {
+                        //console.log(numberOfGrants.labels[context.dataIndex])
+                        //const hiddenIndex = numberOfGrants.labels.indexOf("Subterrânea");
+                        //console.log(hiddenIndex)
+                       /* if (hiddenIndex !== -1) {
+                            numberOfGrants.datasets[0].data.splice(hiddenIndex, 1);
+                            //numberOfGrants.datasets[0].backgroundColor.splice(hiddenIndex, 1);
+                            //numberOfGrants.datasets[0].borderColor.splice(hiddenIndex, 1);
+                          }*/
+                        const originalValue = numberOfGrants.datasets[0].data[context.dataIndex];
+                        return `Qt: ${originalValue}`;
+                    },
+                },
+            },
+        },
+        scale: {
+            ticks: {
+                display: true
+            }
+        },
+    }
 
+    const logarithmicData = numberOfGrants.datasets[0].data.map((value) => Math.log10(value));
 
     useEffect(() => {
         overlays.shapes.map(shape => {
@@ -79,11 +82,11 @@ export function ElemPolarAreaChart() {
                             shape.markers.barragem !== null ? shape.markers.barragem.length : 0
                         ],
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.5)',
-                            'rgba(54, 162, 235, 0.5)',
-                            'rgba(255, 206, 86, 0.5)',
-                            'rgba(75, 192, 192, 0.5)',
-                            'rgba(153, 102, 255, 0.5)'
+                            'rgba(4,12,157,0.5)',
+                            'rgba(1,147,103,0.5)',
+                            'rgba(227,171,0,0.5)',
+                            'rgba(157,4,113, 0.5)',
+                            'rgba(189,176,26,0.5)'
                         ],
                         borderWidth: 1,
                     },
@@ -91,7 +94,7 @@ export function ElemPolarAreaChart() {
 
 
 
-            setMarkersLen(
+            setNumberOfGrants(
                 prevState => ({
                     ...prevState,
                     //datasets: [...prevState.datasets, { data: newData }]
@@ -102,9 +105,14 @@ export function ElemPolarAreaChart() {
     }, [overlays])
 
     useEffect(() => {
-        console.log(markersLen)
-    }, [markersLen])
+        console.log(numberOfGrants.datasets[0])
+    }, [numberOfGrants])
 
 
-    return <PolarArea style={{ maxHeight: 300 }} data={markersLen} />;
+    return <PolarArea style={{ maxHeight: 300 }}
+        data={{
+            ...numberOfGrants,
+            datasets: [{ ...numberOfGrants.datasets[0], data: numberOfGrants.datasets[0].data.map((value) => Math.log10(value)) }],
+        }}
+        options={options} />;
 }
