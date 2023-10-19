5951e341d911220ff07813877357ec7dae1a1d3f
ueredeveloper <ueredeveloper@gmail.com>
2023-10-19 13:42:05 -0300

    feat: implements pagination form grant tables
    Adicionando paginação às tabelas de outorga.

--- src/components/MainFlow/General/GrantsPanel.js
+++ src/components/MainFlow/General/GrantsPanel.js
@@ -5,6 +5,8 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import GrantsTable from "../../Commom/GrantsTable";
 import { useData } from "../../../hooks/analyse-hooks";
+import { Stack } from "@mui/system";
+import { Pagination } from "@mui/material";
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -52,9 +54,14 @@ function GrantsPanel() {
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+    const [page, setPage] = React.useState(1);
+
+    const handleChangePagination = (event, value) => {
+      setPage(value);
+    };
 
     //const [, , overlays, ] = useContext(AnalyseContext);
-    const {overlays} = useData();
+    const { overlays } = useData();
 
     return (
         <Box sx={{ width: "100%" }}>
@@ -67,30 +74,33 @@ function GrantsPanel() {
                     <Tab label="Barragens" {...a11yProps(4)} />
                 </Tabs>
             </Box>
-            {
-                overlays.shapes.map((shape, i) => {
-                    return (
-                        <div id="div-panel" key={i} style={{ height: '500px' }}>
-                            <TabPanel value={value} index={0}>
-                                <GrantsTable name={"subterranea"} markers={shape.markers.subterranea !== null ? shape.markers.subterranea : []} />
-                            </TabPanel>
-                            <TabPanel value={value} index={1}>
-                                <GrantsTable name={"superficial"} markers={shape.markers.superficial !== null ? shape.markers.superficial : []} />
-                            </TabPanel>
-                            <TabPanel value={value} index={2}>
-                                <GrantsTable name={"lancamento_pluviais"} markers={shape.markers.lancamento_pluviais !== null ? shape.markers.lancamento_pluviais : []} />
-                            </TabPanel>
-                            <TabPanel value={value} index={3}>
-                                <GrantsTable name={"lancamento_efluentes"} markers={shape.markers.lancamento_efluentes !== null ? shape.markers.lancamento_efluentes : []} />
-                            </TabPanel>
-                            <TabPanel value={value} index={4}>
-                                <GrantsTable name={"barragem"} markers={shape.markers.barragem !== null ? shape.markers.barragem : []} />
-                            </TabPanel>
-                        </div>
-                    )
-                })
-            }
-
+            <Stack>
+                {
+                    overlays.shapes?.map((shape, i) => {
+                        if (page===++i)
+                        return (
+                            <div id="div-panel" key={i} style={{ height: '500px' }}>
+                                <TabPanel value={value} index={0}>
+                                    <GrantsTable name={"subterranea"} markers={shape.markers.subterranea !== null ? shape.markers.subterranea : []} />
+                                </TabPanel>
+                                <TabPanel value={value} index={1}>
+                                    <GrantsTable name={"superficial"} markers={shape.markers.superficial !== null ? shape.markers.superficial : []} />
+                                </TabPanel>
+                                <TabPanel value={value} index={2}>
+                                    <GrantsTable name={"lancamento_pluviais"} markers={shape.markers.lancamento_pluviais !== null ? shape.markers.lancamento_pluviais : []} />
+                                </TabPanel>
+                                <TabPanel value={value} index={3}>
+                                    <GrantsTable name={"lancamento_efluentes"} markers={shape.markers.lancamento_efluentes !== null ? shape.markers.lancamento_efluentes : []} />
+                                </TabPanel>
+                                <TabPanel value={value} index={4}>
+                                    <GrantsTable name={"barragem"} markers={shape.markers.barragem !== null ? shape.markers.barragem : []} />
+                                </TabPanel>
+                            </div>
+                        )
+                    })
+                }
+            </Stack>
+            <Pagination count={overlays.shapes.length} page={page} onChange={handleChangePagination} />
 
         </Box>
     );
