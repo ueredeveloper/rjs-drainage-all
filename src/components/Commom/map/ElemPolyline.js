cab666305d633db6787fe0b8435c75ebabce6096
William Murcia <130362911+DvWill@users.noreply.github.com>
2025-06-26 12:13:33 +0000

    Restored to '183d7a69835d2aee8b682ed4e623366387bd1c81'
    Replit-Restored-To: 183d7a69835d2aee8b682ed4e623366387bd1c81

--- src/components/Commom/map/ElemPolyline.js
+++ src/components/Commom/map/ElemPolyline.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { createRiverInfoWindowContent } from "./infowindow/ElemRiverInfoWindow";
 
 /**
  * Elemento de Polilinha para as shapes Hidrogeo_Fraturado e Hidrogeo_Poroso.
@@ -8,15 +7,12 @@ import { createRiverInfoWindowContent } from "./infowindow/ElemRiverInfoWindow";
  */
 const ElemPolyline = ({ shape, map }) => {
   const [polyline, setPolyline] = useState();
-  const [infoWindow, setInfoWindow] = useState();
   // Para controlar a largura da linha de acordo com o zoom
   const [strokeWeight, setStrokeWeight] = useState(1.5);
 
   const [strokeColor] = useState(
     "#" + Math.floor(Math.random() * 16777215).toString(16),
   );
-
-  
 
   useEffect(() => {
     // Listener para capturar o zoom e mudar a largura da linha
@@ -32,24 +28,16 @@ const ElemPolyline = ({ shape, map }) => {
     if (!polyline) {
       setPolyline(new window.google.maps.Polyline());
     }
-
-    if (!infoWindow) {
-      setInfoWindow(new window.google.maps.InfoWindow());
-    }
-
     // remove marker from map on unmount
     return () => {
       if (polyline) {
         polyline.setMap(null);
       }
-      if (infoWindow) {
-        infoWindow.close();
-      }
       window.google.maps.event.removeListener(zoomListener);
     };
-  }, [polyline, setPolyline, infoWindow]);
+  }, [polyline, setPolyline]);
 
-  if (polyline && infoWindow) {
+  if (polyline) {
     /**
      * Coverter coordenada postgres para o formato gmaps
      */
@@ -68,30 +56,7 @@ const ElemPolyline = ({ shape, map }) => {
       strokeColor: strokeColor,
       strokeWeight: strokeWeight,
       map: map,
-      clickable: true,
     });
-
-    // Adiciona listener de click para mostrar InfoWindow
-    const clickListener = polyline.addListener("click", (event) => {
-      // Calcula o ponto médio da polyline para posicionar o InfoWindow
-      const path = polyline.getPath();
-      const midIndex = Math.floor(path.getLength() / 2);
-      const position = path.getAt(midIndex);
-
-      // Define o conteúdo do InfoWindow com as informações do rio
-      const content = createRiverInfoWindowContent(shape);
-      
-      infoWindow.setContent(content);
-      infoWindow.setPosition(position || event.latLng);
-      infoWindow.open(map);
-    });
-
-    // Cleanup do listener quando o componente é desmontado
-    return () => {
-      if (clickListener) {
-        window.google.maps.event.removeListener(clickListener);
-      }
-    };
   }
 
   return null;
