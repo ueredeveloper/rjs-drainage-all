e85548f0814ffa9824f55f53892d7306c6480947
Fabrício José Barrozo <fabricio.barrozo@adasa.df.gov.br>
2026-03-25 08:45:41 -0300

    fix: retorna coordenada padrão na adasa

--- src/initials-states.js
+++ src/initials-states.js
@@ -6,8 +6,8 @@ const initialsStates = {
         tp_id: 1,
         // subterrâneo
         ti_id: 2,
-        int_latitude: -15.973561, //-15.775139, ADASA
-        int_longitude: -47.629522 //-47.939599, ADASA
+        int_latitude: -15.775139,  //barragem -15.973561, 
+        int_longitude: -47.939599,  //-47.629522 //
     },
     /* desenhos feitos pelo usuário no mapa (cículos, polígonos, retângulos)*/
     overlays: {
